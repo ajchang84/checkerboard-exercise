beaf29ccea0b0d500f8db6de6cfe6e1382a5440e
Alexander Chang <gxchang@gmail.com>
2016-03-01 15:33:09 -0800

    finished gradient

--- script.js
+++ script.js
@@ -2,18 +2,25 @@
 document.addEventListener('DOMContentLoaded', function(){
 
   function createBoard(tiles){
+    var tile1 = "47ab47";
+    var tile2 = "3232aa";
+
     for (var i = 0; i < tiles; i++) {
-    var div = document.createElement('div');
-    div.style.width = "11.1%";
-    div.style.paddingBottom = "11.1%";
-    div.style.float = "left";
-    div.style.backgroundColor = '#'+[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)]
-                                   +[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)] 
-                                   +[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)] 
-                                   +[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)] 
-                                   +[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)] 
-                                   +[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)]; 
-    document.body.appendChild(div);
+      var div = document.createElement('div');
+      div.style.width = "11.1%";
+      div.style.paddingBottom = "11.1%";
+      div.style.float = "left";
+      if (i % 9 === 0) {
+        tile1 = (parseInt(tile1, 16) + 1500).toString(16);
+        tile2 = (parseInt(tile2, 16) + 2000).toString(16);
+      }
+      if (i % 2 === 0) {
+        div.style.backgroundColor = "#" + tile1;
+      }  
+      else {
+        div.style.backgroundColor = "#" + tile2; 
+      }
+      document.body.appendChild(div);
     }
   }
 

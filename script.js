61d65b0920db8659769af6fdc6f58a9436406d7f
Alexander Chang <gxchang@gmail.com>
2016-03-02 00:17:47 -0800

    added flashing tiles

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 document.addEventListener('DOMContentLoaded', function(){
 
   function createBoard(tiles){
+
     for (var i = 0; i < tiles; i++) {
     var div = document.createElement('div');
     div.style.width = "11.1%";
@@ -18,6 +19,19 @@ document.addEventListener('DOMContentLoaded', function(){
   }
 
   createBoard(99);
+
+  var timerID = setInterval (function(){
+    for (var i = 1; i < 100; i++) {
+    document.body.children[i].style.backgroundColor = '#'+[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)]
+                                                         +[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)] 
+                                                         +[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)] 
+                                                         +[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)] 
+                                                         +[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)] 
+                                                         +[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)];       
+          }
+
+  }, 500);
+
 
 
 });

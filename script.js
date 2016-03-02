bdf4dc99c2068755399111ddc253257fe07d9c7e
Alexander Chang <gxchang@gmail.com>
2016-03-02 00:38:15 -0800

    added music to background

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function(){
 
   function createBoard(tiles){
 
+
+
     for (var i = 0; i < tiles; i++) {
     var div = document.createElement('div');
     div.style.width = "11.1%";
@@ -16,6 +18,13 @@ document.addEventListener('DOMContentLoaded', function(){
                                    +[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)]; 
     document.body.appendChild(div);
     }
+    var audio = document.createElement('audio');
+    audio.setAttribute('autoplay', 'autoplay');
+    document.body.appendChild(audio);
+    var source = document.createElement('source');
+    source.setAttribute('src', 'elevator.mp3');
+    source.setAttribute('type', 'audio/mpeg');
+    document.body.children[100].appendChild(source);
   }
 
   createBoard(99);
